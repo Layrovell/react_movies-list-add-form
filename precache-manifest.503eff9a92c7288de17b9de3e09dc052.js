927d115d9b23df4bdf25b53bbf0297b7157b60b3
Layrovell <marina95prodan@gmail.com>
2021-01-25 11:26:18 +0200

    Updates

---
+++ precache-manifest.503eff9a92c7288de17b9de3e09dc052.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c35bd1e8ac36f078889adde19092a46",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0f6e81927a9e6250b114",
+    "url": "./static/css/2.9634ee28.chunk.css"
+  },
+  {
+    "revision": "ed99064f97911659ba1c",
+    "url": "./static/css/main.6e29da76.chunk.css"
+  },
+  {
+    "revision": "0f6e81927a9e6250b114",
+    "url": "./static/js/2.51a060dc.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.51a060dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed99064f97911659ba1c",
+    "url": "./static/js/main.d1068883.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.503eff9a92c7288de17b9de3e09dc052.js"
 );
 
 self.addEventListener('message', (event) => {
